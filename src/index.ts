c2d74c8a8958fe5f9071d6c48374aad241af2edf
rakshitkamboj <rakshitkamboj123@gmail.com>
2023-05-01 15:12:45 +0530

    added logger file

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+//entry point for compiler
+
+
+export class Log{
+    static success(msg:string){
+        console.log(`%c ${msg}`,'color:green')
+
+    }
+    static error(msg:string){
+        console.log(`%c ${msg}`,'color:red')
+        
+    }
+    static info(msg:string){
+        console.log(`%c ${msg}`,'color:black,background:yellow')
+        
+    }
+}
